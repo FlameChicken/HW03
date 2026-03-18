5bd4b9590b1ffe72e2ed4ad04153afb95ead7202
flameChicken <dnl01090@gmail.com>
2026-03-18 20:22:23 +0900

    Create HW03 project files

---
+++ HW03/HW03.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+class Inventory
+{
+private:
+    T* pItems_;
+    int capacity_;
+    int size_;
+
+public:
+    Inventory(int capacity = 10)
+    {
+        if (capacity <= 0)
+            capacity_ = 1;
+        else
+            capacity_ = capacity;
+
+        size_ = 0;
+        pItems_ = new T[capacity_];
+    }
+
+    ~Inventory()
+    {
+        delete[] pItems_;
+        pItems_ = nullptr;
+    }
+
+    void AddItem(const T& item)
+    {
+        if (size_ >= capacity_)
+        {
+            cout << "인벤토리가 꽉 찼습니다!" << endl;
+            return;
+        }
+
+        pItems_[size_] = item;
+        size_++;
+    }
+
+    void RemoveLastItem()
+    {
+        if (size_ <= 0)
+        {
+            cout << "인벤토리가 비어있습니다." << endl;
+            return;
+        }
+
+        size_--;
+    }
+
+    int GetSize() const
+    {
+        return size_;
+    }
+
+    int GetCapacity() const
+    {
+        return capacity_;
+    }
+
+    void PrintAllItems() const
+    {
+        if (size_ == 0)
+        {
+            cout << "비어있음" << endl;
+            return;
+        }
+
+        for (int i = 0; i < size_; i++)
+        {
+            pItems_[i].PrintInfo();
+        }
+    }
+};
+
+class Item
+{
+private:
+    string name_;
+    int price_;
+
+public:
+    Item(string name = "None", int price = 0)
+        : name_(name), price_(price) {}
+
+    void PrintInfo() const
+    {
+        cout << "[이름: " << name_
+            << ", 가격: " << price_ << "G]" << endl;
+    }
+};
+
+int main()
+{
+    Inventory<Item> inventory(3);
+
+    inventory.AddItem(Item("검", 100));
+    inventory.AddItem(Item("방패", 150));
+    inventory.AddItem(Item("포션", 50));
+
+    inventory.PrintAllItems();
+
+    inventory.RemoveLastItem();
+    inventory.PrintAllItems();
+
+    return 0;
+}
